97bde214495dbb98084752bd5f371aff2a763d81
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-05 23:34:47 +0530

    importing the names.js to app.js using module.exports

---
+++ modules/app.js
@@ -0,0 +1,6 @@
+const names = require("./names");
+console.log(names);
+
+// sayHi("rangaraju");
+// sayHi(john);
+// sayHi(peter);
---
+++ modules/names.js
@@ -0,0 +1,9 @@
+//global
+const john = "john";
+const peter = "peter";
+//local
+const secret = "super secret";
+
+module.exports = { john, peter };
+
+// console.log(module);
