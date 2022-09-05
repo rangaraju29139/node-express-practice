6227a3923194903f284718121d15e7133893e75e
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-06 00:26:02 +0530

    added the os info with currestOs custom object

--- builtInModules/osModule/app.js
+++ builtInModules/osModule/app.js
@@ -3,3 +3,11 @@ const user = os.userInfo();
 console.log(user);
 
 console.log(` the uptime is ${os.uptime()} seconds`);
+
+const currentOs = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOs);
