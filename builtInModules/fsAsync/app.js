bee6a0bc5fe6b2facf9830f6bfc36832b2f44400
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-07 01:56:11 +0530

    Async writeFile using nested callbacks

--- builtInModules/fsAsync/app.js
+++ builtInModules/fsAsync/app.js
@@ -5,5 +5,25 @@ readFile("./content/data.txt", "utf-8", (err, result) => {
     console.log(err);
     return;
   }
-  console.log(result);
+  //   console.log(result);
+  const first = result;
+  readFile("./content/output.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = data;
+    writeFile(
+      "./content/AsyncResult.txt",
+
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if ((err, result)) {
+          console.log(err);
+          return;
+        }
+        console.log(`write to the file completed`);
+      }
+    );
+  });
 });
