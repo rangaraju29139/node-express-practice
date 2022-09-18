518963c707239ee06a88e9735e783546956bb282
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-18 21:18:20 +0530

    added coditions to handle the request of different pages

--- builtInModules/httpModule/app.js
+++ builtInModules/httpModule/app.js
@@ -1,9 +1,21 @@
 const http = require("http");
 
+// const server = http.createServer((req, res) => {
+//   console.log(req);
+//   res.write("Welcome to my Home Page");
+//   res.end();
+// });
+
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.write("Welcome to my Home Page");
-  res.end();
+  if (req.url === "/") {
+    res.end("this is the home page");
+  }
+  if (req.url === "/about") {
+    res.end("this is the about page");
+  }
+  res.end(`
+  <h1>Oops!! we are not able to find the page you are looking for....</h1>
+  `);
 });
 
 server.listen(5000);
