3557f3acd7c19cb192c5e0033e87e99503a7d165
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-20 21:08:24 +0530

    stream read using utf8 encoding

--- streams/app.js
+++ streams/app.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 const filePath = path.resolve(__dirname, "large-file.txt");
 
-const stream = createReadStream(filePath);
+// const stream = createReadStream(filePath);
+const stream = createReadStream(filePath, { encoding: "utf8" });
 
 stream.on("data", (result) => {
   console.log(result);
