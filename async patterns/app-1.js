bef17ec3d554592fd21031461d38cf4e043e2dba
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-20 15:55:40 +0530

    added the readfile using promise

--- async patterns/app-1.js
+++ async patterns/app-1.js
@@ -1,10 +1,19 @@
+const { rejects } = require("assert");
 const { readFile } = require("fs");
+const { resolve } = require("path");
 
-readFile("./async patterns/content/first.txt", "utf8", (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log(data);
-  }
-});
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./async patterns/content/first.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
