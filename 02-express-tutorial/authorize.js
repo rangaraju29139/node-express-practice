e148f75ea6957799a09dcf1a67fc9beb8776045c
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 21:23:41 +0530

    enhanced authorized function

--- 02-express-tutorial/authorize.js
+++ 02-express-tutorial/authorize.js
@@ -1,5 +1,12 @@
 const authorize = (req, res, next) => {
   console.log("authorize");
+  const { user } = req.query;
+  if (user === "rangaraju") {
+    res.send(`Welcome ${user}`);
+  } else {
+    res.status(401).send(`unauthorized`);
+  }
+
   next();
 };
 
