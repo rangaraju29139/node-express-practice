9dabbbce447478cc6873ede54ab91d5809e87bb2
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 22:06:34 +0530

    imported the static files using app.use(express.static())

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,33 +1,11 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger.js");
-const authorize = require("./authorize.js");
+let { people } = require("./data.js");
 
-// req => middleware => res
-// middle ware to all the functions
-// app.use(logger);
+app.use(express.static("./methods-public"));
 
-// middle ware for certain pattern
-
-// app.use("/api", logger);
-//this is applicable to all the request of /api and sub children
-
-//order matters
-app.use([authorize, logger]);
-
-app.get("/", (req, res) => {
-  res.send("home page");
-});
-
-// middle ware to all the functions below the app.use and the middle ware not applicable to /
-// app.use(logger);
-
-app.get("/about", (req, res) => {
-  res.send("about page");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("products");
+app.get("/api/people", (req, res) => {
+  res.json({ success: true, data: people });
 });
 
 app.listen(5000, () => {
