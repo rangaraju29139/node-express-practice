15ac4995cf939a81449be4a50ed53af4ae63a601
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 16:19:59 +0530

    hosted the navbar-app using node server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,15 +1,33 @@
 const http = require("http");
+const { readFileSync, read } = require("fs");
+
+const homePage = readFileSync("./navbar-app/index.html");
+const styles = readFileSync("./navbar-app/styles.css");
+const homeImg = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   console.log("user hit the server");
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write(`<h1>Home page</h1>`);
+    res.write(homePage);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(`<h1>About page</h1>`);
+    res.end();
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(styles);
+    res.end();
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "text/svg+xml" });
+    res.write(homeImg);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
   } else {
     res.writeHead(404, { "content-type": "text/html" });
