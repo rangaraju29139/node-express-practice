b82a75e7d98587be84f9cfffe486133029216ae8
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 15:28:25 +0530

    updated as text/plain

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("user hit the server");
-  res.writeHead(200, { "content-type": text / html });
+  res.writeHead(200, { "content-type": "text/plain" });
   res.write(`<h1>Simple server</h1>`);
   res.end();
 });
