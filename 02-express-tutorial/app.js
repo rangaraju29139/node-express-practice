ff42041f8c7a836b71245de70f8726280413de08
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 20:56:13 +0530

    adding the middleware to the multiple routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,18 +3,19 @@ const app = express();
 
 // req => middleware => res
 
-const logger = (req, res) => {
+const logger = (req, res, next) => {
   const method = req.method;
   const url = req.url;
   const time = new Date().getFullYear();
   console.log(method, url, time);
+  next();
 };
 
 app.get("/", logger, (req, res) => {
   res.send("home page");
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", logger, (req, res) => {
   res.send("about page");
 });
 
