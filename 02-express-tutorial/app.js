a226e61e8b802796a2474ce5b8d7f84663b6dbc7
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 15:41:59 +0530

    hanling diff req url's

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,11 +2,20 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("user hit the server");
-  console.log(req.method);
-  console.log(req.url);
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write(`<h1>Simple server</h1>`);
-  res.end();
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1>Home page</h1>`);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1>About page</h1>`);
+    res.end();
+  } else {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1>page not found</h1>`);
+    res.end();
+  }
 });
 
 server.listen(5000);
