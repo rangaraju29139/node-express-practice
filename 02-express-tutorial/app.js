6d61ca9fad9527133e1badff7546b2bbc76a00b7
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 15:40:04 +0530

    demonstrating the req.method and req.url

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("user hit the server");
-  res.writeHead(200, { "content-type": "text/plain" });
+  console.log(req.method);
+  console.log(req.url);
+  res.writeHead(200, { "content-type": "text/html" });
   res.write(`<h1>Simple server</h1>`);
   res.end();
 });
