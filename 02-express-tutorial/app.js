c02a66cdab7a7d752fc57b39e3a6b1de96163e12
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-29 16:48:21 +0530

    simple express functionality with express and sending backing the status codes along with response

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,39 +1,26 @@
-const http = require("http");
-const { readFileSync, read } = require("fs");
+const express = require("express");
+const app = express();
 
-const homePage = readFileSync("./navbar-app/index.html");
-const styles = readFileSync("./navbar-app/styles.css");
-const homeImg = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(`<h1>About page</h1>`);
-    res.end();
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(styles);
-    res.end();
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "text/svg+xml" });
-    res.write(homeImg);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write(`<h1>page not found</h1>`);
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.status(200).send(`<h1>Home page</h1>`);
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  res.status(200).send(`<h1>About Page</h1>`);
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send(`<h1>Page not found</h1>`);
+});
+
+app.listen(5000, () => {
+  console.log("server is listening at port 5000");
+});
+
+// app.get
+// app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
