534b3da4cee662094166e38435f4ee192706b4cf
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-30 21:36:44 +0530

    refactoring the routes using the router.route

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -10,14 +10,14 @@ const {
   createNamePostMan,
 } = require("../controllers/people.js");
 
-router.get("/", getPeople);
+// router.get("/", getPeople);
+// router.post("/", createName);
+// router.post("/postman", createNamePostMan);
+// router.put("/:id", updateName);
+// router.delete("/:id", deleteName);
 
-router.post("/", createName);
-
-router.post("/postman", createNamePostMan);
-
-router.put("/:id", updateName);
-
-router.delete("/:id", deleteName);
+router.route("/").get(getPeople).post(createName);
+router.route("/:id").put(updateName).delete(deleteName);
+router.route("/postman").post(createNamePostMan);
 
 module.exports = router;
