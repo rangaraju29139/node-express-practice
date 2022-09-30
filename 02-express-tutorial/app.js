ebcde43851f0f48b49c47a404a75fd9d1d0eb518
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-30 20:12:55 +0530

    added the functionality to handle the put http request

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -20,12 +20,44 @@ app.post("/api/people", (req, res) => {
   }
 });
 
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide the name!!" });
+  }
+  res.status(201).json({ success: true, person: [...people, name] });
+});
+
 app.post("/login", (req, res) => {
   console.log(req.body);
   if (req.body.name) {
     res.send(`Welcome ${req.body.name}`);
   } else {
     res.send("You have submitted a empty form ");
+  }
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const personName = people.find((person) => person.id === Number(id));
+  console.log(personName);
+  if (personName) {
+    const newPeople = people.map((person) => {
+      if (person.id === Number(id)) {
+        person.name = name;
+      }
+      return person;
+    });
+    return res.status(200).json({ success: true, data: newPeople });
+  } else {
+    return res.json({
+      success: false,
+      msg: "could not find the person with given id",
+    });
   }
 });
 
