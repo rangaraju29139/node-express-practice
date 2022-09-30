a209947fb8f6e3ddc9c8d98896f759f1e886d90b
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-30 18:08:50 +0530

    sending and handling the post request using axios and express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,9 +4,20 @@ let { people } = require("./data.js");
 
 app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  if (!name) {
+    res.status(400).json({ success: false, msg: "Please provide the name!!" });
+  } else {
+    res.status(201).json({ success: true, person: name });
+  }
 });
 
 app.post("/login", (req, res) => {
