6d8f8cb1840da7dd6a61854e7d182f5e0cb7a8c7
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-30 17:35:44 +0530

    handling the post request using the urlEncoded middle ware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,9 +3,19 @@ const app = express();
 let { people } = require("./data.js");
 
 app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/people", (req, res) => {
   res.json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  if (req.body.name) {
+    res.send(`Welcome ${req.body.name}`);
+  } else {
+    res.send("You have submitted a empty form ");
+  }
 });
 
 app.listen(5000, () => {
